8232a1f3225fbdc462d10cc4fa311065233cadb5
Ramy Mohamed <mohamer@algonquincollege.com>
2024-11-26 19:44:19 -0500

    Change clolr

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #b51b0ad7;
+  background-color: #3c673cd7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
