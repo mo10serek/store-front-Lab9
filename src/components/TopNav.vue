4a14dbf7c32ecdbe8d50a664a98355881a31be1b
MichaelBalcerzak <balcerzak.michael@gmail.com>
2024-12-03 22:40:24 -0500

    change to light blue

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #3507ddd7;
+  background-color: #00e1ffd7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
